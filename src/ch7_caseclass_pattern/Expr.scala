52ee68521636eec1769bd111926ca792aeb52301
peterzd <zdsunshine0640@gmail.com>
2012-04-24 21:31:57 +0800

    ch7

---
+++ src/ch7_caseclass_pattern/Expr.scala
@@ -0,0 +1,15 @@
+package ch7_caseclass_pattern
+
+abstract class Expr {
+
+  def eval: Int = this match {
+    case Number(n) => n
+    case Sum(e1, e2) => e1.eval + e2.eval
+
+  }
+
+}
+case class Number(n: Int) extends Expr
+case class Sum(e1: Expr, e2: Expr) extends Expr
+
+
